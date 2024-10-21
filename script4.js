905493b0baf1ce7fabbe696f382b00d3d3f63966
albalab <albalab@yandex.ru>
2024-10-21 23:48:48 +0400

    iframe

--- script4.js
+++ script4.js
@@ -1,12 +1,11 @@
 (function() {
     'use strict';
 
-    // Создаем новый iframe элемент
     const iframe = document.createElement('iframe');
     iframe.setAttribute('ref', 'chartIframe');
     iframe.src = 'http://127.0.0.1:8080/#/candles-chart-iframe';
 
-// Устанавливаем стили для фиксированного позиционирования
+    // Устанавливаем стили для фиксированного позиционирования
     iframe.style.position = 'fixed'; // Фиксированное позиционирование
     iframe.style.top = '0';           // Отступ сверху
     iframe.style.left = '0';          // Отступ слева
@@ -15,16 +14,6 @@
     iframe.style.border = 'none';     // Убираем границу
     iframe.style.zIndex = '1000';     // Устанавливаем высокий z-index
 
-// Вставляем iframe в конец body
+    // Вставляем iframe в конец body
     document.body.appendChild(iframe);
-
-// Функция для отправки сообщения в iframe
-    function sendMessage() {
-        const message = { time: new Date().toISOString(), data: "Your message here" }; // Ваши данные
-        iframe.contentWindow.postMessage(message, '*'); // Отправка сообщения в iframe
-    }
-
-// Устанавливаем интервал для отправки сообщения каждые 5 секунд
-    setInterval(sendMessage, 5000);
-
 })();
