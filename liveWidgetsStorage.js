700357068c1f3b78fe3001e8f4ded02959167c0f
albalab <albalab@yandex.ru>
2024-11-05 14:18:41 +0300

    global

--- liveWidgetsStorage.js
+++ liveWidgetsStorage.js
@@ -2,7 +2,7 @@
     'use strict';
 
     // Проверка и инициализация widgetsStorage
-    global.widgetsStorage = global.widgetsStorage || {};
+    global.widgetsStorage = global?.widgetsStorage || {};
 
 
     const enabledWidgets = [
