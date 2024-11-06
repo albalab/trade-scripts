5b0d8bdd47f205a32a912b11c122eca6c0d19ba9
albalab <albalab@yandex.ru>
2024-11-06 14:50:23 +0300

    Storage

--- templates/createLimitOrder.js
+++ templates/createLimitOrder.js
@@ -1,11 +1,11 @@
 (function () {
     'use strict';
 
-    setInterval(() => {
+    setTimeout(() => {
 
-        if(window.createLimitOrder) {
-            window.createLimitOrder();
-        }
+        // Пример вызова метода с фронта
+        window.sendLimitOrder(1, 1150, 'CHMF', 'MOEX', 'buy', 'D88141');
+
 
     }, 5000);
 
--- templates/initLimitOrder.js
+++ templates/initLimitOrder.js
@@ -1,8 +1,32 @@
-(function () {
+(function() {
     'use strict';
 
-    window.createLimitOrder = function () {
-        console.log('FAKE LIMIT ORDER CREATED');
-    };
+    window.sendLimitOrder = async function(volume, price, ticker, exchange, side, portfolio) {
+        const orderDetails = {
+            side: side,
+            quantity: volume,
+            price: price,
+            instrument: {
+                symbol: ticker,
+                exchange: exchange,
+                instrumentGroup: "TQBR"
+            },
+            user: { portfolio: portfolio },
+            timeInForce: "oneday",
+            comment: "Отправлено с фронта"
+        };
 
+        try {
+            const response = await fetch('http://localhost:8123/limit-order', {
+                method: 'POST',
+                headers: { 'Content-Type': 'application/json' },
+                body: JSON.stringify(orderDetails)
+            });
+            const data = await response.json();
+            console.log("Ответ сервера:", data);
+        } catch (error) {
+            console.error("Ошибка при отправке лимитного ордера:", error.message);
+        }
+    };
+    
 })();
