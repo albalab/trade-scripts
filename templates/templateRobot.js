f3c7e5b0c86e86a826a336cd951985bc7ca211ad
albalab <albalab@yandex.ru>
2024-11-06 16:11:40 +0300

    Storage

--- templates/templateRobot.js
+++ templates/templateRobot.js
@@ -8,23 +8,33 @@
     if (!allowedTickers.includes(triggerTicker)) return;
 
 
-    setInterval(() => {
+    /*setInterval(() => {
         console.log(window.alorTrade);
-    }, 3000);
+    }, 3000);*/
 
-    // Создаем прокси для window.alorTrade
-    window.alorTrade = new Proxy(window.alorTrade || {}, {
-        set(target, property, value) {
-            // Устанавливаем новое значение свойства
-            target[property] = value;
+    const checkAlorTradeInterval = setInterval(() => {
+        // Проверяем доступность window.alorTrade
+        if (window.alorTrade) {
+            // Как только объект становится доступен, устанавливаем прокси
+            window.alorTrade = new Proxy(window.alorTrade, {
+                set(target, property, value) {
+                    // Устанавливаем новое значение свойства
+                    target[property] = value;
 
-            // Выводим обновленный объект в консоль
-            console.log('alorTrade обновлен:', target);
+                    // Выводим обновленный объект в консоль
+                    console.log('alorTrade обновлен:', target);
 
-            // Сообщаем об успешной записи значения
-            return true;
+                    // Сообщаем об успешной записи значения
+                    return true;
+                }
+            });
+
+            // Очищаем таймер, так как прокси уже установлен
+            clearInterval(checkAlorTradeInterval);
+            console.log("Прокси для alorTrade установлен.");
         }
-    });
+    }, 1000); // Проверяем каждые 100 миллисекунд
+
 
 
     //window.sendLimitOrder(1, 1150, 'CHMF', 'MOEX', 'buy', 'D88141');
