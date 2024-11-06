d06cca2c37f405392a5ef3614c8ad8318c1f2066
albalab <albalab@yandex.ru>
2024-11-06 16:08:09 +0300

    Storage

--- templates/templateRobot.js
+++ templates/templateRobot.js
@@ -8,6 +8,23 @@
     if (!allowedTickers.includes(triggerTicker)) return;
 
 
+
+
+    // Создаем прокси для window.alorTrade
+    window.alorTrade = new Proxy(window.alorTrade || {}, {
+        set(target, property, value) {
+            // Устанавливаем новое значение свойства
+            target[property] = value;
+
+            // Выводим обновленный объект в консоль
+            console.log('alorTrade обновлен:', target);
+
+            // Сообщаем об успешной записи значения
+            return true;
+        }
+    });
+
+
     //window.sendLimitOrder(1, 1150, 'CHMF', 'MOEX', 'buy', 'D88141');
 
 })();
